4d71fa1a50d201837d266bcf1c2b3d37ad94d443
shpacfc <kerry_duff@hotmail.com>
2026-06-30 12:44:17 +0100

    Add click event to button for alert message

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+// SH PAC FC Website
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const button = document.querySelector(".button");
+
+    if(button){
+
+        button.addEventListener("click", function(e){
+
+            e.preventDefault();
+
+            alert("Thank you for your interest!\n\nBooking for FREE trials will be available soon.\n\nFor now please email:\n\ninfo@shpacfc.com");
+
+        });
+
+    }
+
+});
